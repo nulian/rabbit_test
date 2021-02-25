9afc98fbd50030c2e7615b98c7c3dfc8680ff8d4
Peter <peter.arentsen@bettyblocks.com>
2021-02-25 13:07:59 +0100

    increase error reporting of lager

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,8 @@ use Mix.Config
 
 config :amqp,
        connection: [host: "127.0.0.1", port: 5672, username: "user", password: "password", virtual_host: "/vhost"]
+
+config :lager,
+       handlers: [
+         level: :debug
+       ]
